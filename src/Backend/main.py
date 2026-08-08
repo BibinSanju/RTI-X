068c3cd61b1b8f8d445bb2947dd6eecbfe95cc64
ANIRUDHAN C <cvbanirudhan@gmail.com>
2026-08-08 15:51:00 +0530

    Initial Backend Server2

--- src/Backend/main.py
+++ src/Backend/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from database import PIODirectory,RTITelemetryLog,get_db
-
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def check_health():
